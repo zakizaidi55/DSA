69f3a38c06705c723b3bf2e7738fac8d0ad65414
zakizaidi55 <zakizaidi55@gmail.com>
2023-09-16 13:06:28 +0530

    Adding new method using XOR

--- missingNumber.cpp
+++ missingNumber.cpp
@@ -12,3 +12,25 @@ public:
         return ans;
     }
 };
+
+
+// -----------------------------Method2-----------------------------------
+class Solution {
+public:
+    int missingElementByXOR(vector<int>& nums) {
+      int ans = 0;
+      // step1: XOR all the values of array
+      for(int i=0; i<nums.size(); i++) {
+        ans ^= nums[i];
+      }
+
+      // Step2: Xor all the values from 0 to n
+      for(int i=0; i<=nums.size(); i++) {
+        ans ^= i;
+      }
+      return ans;
+    }
+    int missingNumber(vector<int>& nums) {
+        return missingElementByXOR(nums);
+    }
+};
