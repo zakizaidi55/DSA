5de2de6dadf39c1261f179dcc1697b5630aaa5d3
zakizaidi55 <zakizaidi55@gmail.com>
2023-09-16 20:50:59 +0530

    Pivot index in an array method 2

--- pivotIndex.cpp
+++ pivotIndex.cpp
@@ -22,3 +22,33 @@ public:
         return -1;
     }
 };
+
+
+// ------------------Method 2------------------------------------
+
+class Solution {
+public:
+    int prefixSumApproach(vector<int>& nums) {
+        vector<int> lSum(nums.size(), 0);
+        vector<int> rSum(nums.size(), 0);
+        // calculte left sum array 
+        for(int i=1; i<nums.size(); i++) {
+            lSum[i] = lSum[i-1] + nums[i-1];
+        }
+        // calculate right sum array
+        for(int i = nums.size()-2; i>=0; i--) {
+            rSum[i] = rSum[i+1] + nums[i+1];
+        }
+
+        for(int i=0; i<nums.size(); i++) {
+            if(lSum[i] == rSum[i])
+                return i;
+        }
+
+        return -1;
+    }
+
+    int pivotIndex(vector<int>& nums) {
+        return prefixSumApproach(nums);
+    }
+};
