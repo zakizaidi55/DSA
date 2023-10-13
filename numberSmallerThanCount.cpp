7cc99f70ee2f6c36bade01be68cabb2600cbd1bb
zakizaidi55 <zakizaidi55@gmail.com>
2023-10-13 14:35:19 +0530

    how many number are smaller in the array

---
+++ numberSmallerThanCount.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        vector<int> ans;
+        int count = 0;
+        for(int i=0; i<nums.size(); i++) {
+            
+            for(int j=0; j<nums.size(); j++) {
+                if(nums[j] < nums[i]) {
+                    count++;
+                }
+            }
+
+            ans.push_back(count);
+            count = 0;
+        }
+
+        return ans;
+    }
+};
