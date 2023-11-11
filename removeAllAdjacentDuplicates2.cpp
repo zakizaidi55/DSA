8c97c801553762f14cba7fce1eecfff1b5df1c16
zakizaidi55 <zakizaidi55@gmail.com>
2023-11-11 19:36:47 +0530

    Add 2nd method

--- removeAllAdjacentDuplicates2.cpp
+++ removeAllAdjacentDuplicates2.cpp
@@ -34,3 +34,35 @@ public:
         return ans;
     }
 };
+
+
+
+
+
+
+// ++++++++++++++++++++++++++=Method 2+++++++++++++++++++++++++++++++++++++
+
+class Solution {
+public:
+    string removeDuplicates(string s, int k) {
+        int i = 0;
+        int j = 0;
+        int n = s.size();
+        vector<int>count(n);
+
+        while(j < n) {
+            s[i] = s[j];
+            count[i] = 1;
+            if(i > 0 && s[i] == s[i-1])
+                count[i] += count[i-1];
+            
+            if(count[i] == k)
+                i -= k;
+            
+            i++;
+            j++;
+        }
+
+        return s.substr(0,i);
+    }
+};
