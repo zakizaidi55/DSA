3377a30287dce34075795df2da280b26c18efe7b
zakizaidi55 <zakizaidi55@gmail.com>
2023-11-13 16:58:32 +0530

    Merge two sorted Linked list

---
+++ linked list/merge2SortelLL.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* left = list1;
+        ListNode* right = list2;
+
+        if(left == 0)
+            return right;
+
+        if(right == 0)
+            return left;
+
+        ListNode* ans = new ListNode(-1);
+        ListNode* mptr = ans;
+        while(left && right) {
+            if(left->val <= right->val) {
+                mptr->next = left;
+                mptr = left;
+                left = left->next;
+            }
+
+            else {
+                mptr->next = right;
+                mptr = right;
+                right = right->next;
+            }
+        }
+
+        // while(left) {
+        //     mptr->next = left;
+        //     mptr = left;
+        //     left = left->next;
+        // }
+
+        // while(right) {
+        //     mptr->next = right;
+        //     mptr = right;
+        //     right = right->next;
+        // } 
+
+        if(left) {
+            mptr->next = left;
+            
+        }
+
+        if(right) {
+            mptr->next = right;
+        } 
+
+        // catch-> to make sure we can made a mistake
+        return ans->next;
+    }
+};
