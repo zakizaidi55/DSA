103edf664c676adfb9c24589e2e5268761087d4b
zakizaidi55 <zakizaidi55@gmail.com>
2023-11-28 17:58:21 +0530

    Check If Word Is Valid After Substitutions

---
+++ Stack/checkIfWordIsValidAfterSubstitutions.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if(s[0] != 'a') {
+            return false;
+        }
+
+        stack<char>st;
+        for(char ch: s) {
+            if(ch == 'a') {
+                st.push(ch);
+            }
+
+            else if(ch == 'b' ) {
+                if(!st.empty() && st.top() == 'a') {
+                    st.push(ch);
+                }
+
+                else {
+                    return false;
+                }
+            }
+
+            else {
+                // ch == c
+                if(!st.empty() && st.top() == 'b') {
+                    st.pop();
+                    if(!st.empty() && st.top() == 'a') {
+                        st.pop();
+                    }
+                    
+                    else {
+                        return false;
+                    }
+                }
+
+                else {
+                    // it means we did'nt find b on the top
+                    return false;
+                }
+            }
+        }
+
+        return st.empty();
+    }
+};
