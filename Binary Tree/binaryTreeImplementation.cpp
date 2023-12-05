a3b1cfc334fbe5b1d158a53513725f6e769210a2
zakizaidi55 <zakizaidi55@gmail.com>
2023-12-05 12:17:25 -0800

    add create bst from inorder and preorder

--- Binary Tree/binaryTreeImplementation.cpp
+++ Binary Tree/binaryTreeImplementation.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<map>
 #include<queue>
 
 using namespace std;
@@ -137,12 +138,68 @@ int height(Node* root) {
     return ans;
 }
 
+int searchInorder(int inorder[],int size, int element) {
+    for(int i=0; i<size; i++) {
+        if(inorder[i] == element)
+            return i;
+    }
+
+    return -1;
+}
+
+void createMapping(int inorder[],int size, map<int, int> &valueToIndexMap) {
+    for(int i=0; i<size; i++) {
+        int element = inorder[i];
+        int index = i;
+        valueToIndexMap[element] = index;
+    }
+}
+
+// sabse important path h preorder ko by ref pass karna padega
+Node* constructTreeFromInorderAndPreorder(int preorder[], int inorder[],int& preIndex, int inorderStart, map<int, int>&valueToIndexMap, int inorderEnd, int size ){
+    // base case
+    if(preIndex >= size || inorderStart > inorderEnd) {
+        return NULL;
+    }
+
+    // 1 case solve
+    int element = preorder[preIndex];
+    preIndex++;
+
+    Node* root = new Node(element);
+
+    // element->search in order
+    // int position = searchInorder(inorder,size, element);
+
+    int position = valueToIndexMap[element];
+
+    root->left = constructTreeFromInorderAndPreorder(preorder, inorder, preIndex, inorderStart, valueToIndexMap, position-1, size);
+    root->right = constructTreeFromInorderAndPreorder(preorder, inorder, preIndex, position+1, valueToIndexMap, inorderEnd, size);
+
+
+    return root;    
+
+}
+
 int main() {
 
-    Node* root = createTree();
+    // Node* root = createTree();
     // levelOrderTraversal(root);
 
-    cout << height(root) << endl;
+    // cout << height(root) << endl;
 
+    int inorder[] = {10,8,6,2,4,12};
+    int preorder[] = {2,8,10,6,4,12};
+
+    int size = 6;
+    int preorderIndex = 0;
+    int inorderStart = 0;
+    int inorderEnd = 5;
+    map<int, int> valueToIndexMap;
+    createMapping(inorder, size, valueToIndexMap);
+    Node* root = constructTreeFromInorderAndPreorder(preorder, inorder, preorderIndex, inorderStart,valueToIndexMap, inorderEnd, size);
+
+    cout <<"printing entire tree" << endl;
+    levelOrderTraversal(root);
     return 0;
 }
