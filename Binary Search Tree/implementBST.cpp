70e7aef3484ea721b61c700a15a6cd98a0a51ac0
zakizaidi55 <zakizaidi55@gmail.com>
2023-12-08 15:11:43 +0530

    Delete in a BST

--- Binary Search Tree/implementBST.cpp
+++ Binary Search Tree/implementBST.cpp
@@ -117,6 +117,19 @@ Node* minValue(Node* root) {
     return temp;
 }
 
+Node* maxValue(Node* root) {
+    if(root == NULL ) {
+        return NULL;
+    }
+
+    Node* temp = root;
+    while(temp->right != NULL) {
+        temp = temp->right;
+    }
+
+    return temp;
+}
+
 bool searchInBST(Node* root, int target) {
     // base case
     if(root == NULL)
@@ -139,6 +152,54 @@ bool searchInBST(Node* root, int target) {
     return leftAns || rightAns;
 }
 
+Node* deleteFromBST(Node* root, int target) {
+    // cout << "inside the delete call " << endl;
+    // cout <<" target " << target << endl;
+    // target ko find and delete
+    if(root == NULL) {
+        return NULL;
+    }
+
+    if(root->data == target ) {
+        if(root->left == NULL && root->right == NULL) {
+            return NULL;
+        }
+
+        else if(root->left != NULL && root->right == NULL) {
+            Node* childSubTree = root->left;
+            delete root;
+            return childSubTree;
+        }
+
+        else if(root->left == NULL && root->right != NULL) {
+            Node* chidlSubTree = root->right;
+            delete root;
+            return chidlSubTree;
+        }
+
+        else {
+            // step1 find the max value from left subtree
+            Node* maxi =  maxValue(root->left);
+            // replacement
+            root->data = maxi->data;
+
+            // delete actual maxi node
+            root->left = deleteFromBST(root->left, maxi->data);
+            return root;
+        }
+    }
+
+    else if(root->data > target) {
+        root->left = deleteFromBST(root->left, target);
+    }
+
+    else {
+        root->right = deleteFromBST(root->right, target);
+    }
+
+    return root;
+}
+
 int main() {
 
     Node* root = NULL;
@@ -146,12 +207,15 @@ int main() {
 
     levelOrderTraversal(root);
 
-    cout <<endl << "printing inorder: "<< endl;
-    inorder(root);
-    cout << endl << "printing postorder " << endl;
-    postorder(root);
-    cout << endl << "printing preorder: " << endl;
-    preorder(root);
-
+    // cout <<endl << "printing inorder: "<< endl;
+    // inorder(root);
+    // cout << endl << "printing postorder " << endl;
+    // postorder(root);
+    // cout << endl << "printing preorder: " << endl;
+    // preorder(root);
+    int target = 20;
+    deleteFromBST(root, target);
+    cout << "after Delete " << endl;
+    levelOrderTraversal(root);
     return 0;
 }
