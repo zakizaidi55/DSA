5037f077af474b92d954a61fe34a87efe9245602
zakizaidi55 <zakizaidi55@gmail.com>
2023-12-15 16:03:05 +0530

    balanced bonary tree method 2

--- Binary Tree/balancedBinaryTree.cpp
+++ Binary Tree/balancedBinaryTree.cpp
@@ -48,3 +48,52 @@ public:
         }
     }
 };
+
+
+
+
+
+
+
+// ------------------method 2----------------------
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalancedNode = true;
+    int height(TreeNode* root) {
+        if(root == NULL) {
+           return 0;
+        } 
+
+       
+        int leftHeight = height(root->left);
+        
+        int rightHeight = height(root->right);
+        
+        // check for all the node, is it balanced
+
+        if(isBalancedNode && abs(leftHeight - rightHeight) > 1) {
+            isBalancedNode = false;
+        }
+
+        int ans = 1 + max(leftHeight, rightHeight);
+
+        
+        return ans;
+    }
+    bool isBalanced(TreeNode* root) {
+        height(root);
+        return isBalancedNode;
+    }
+};
