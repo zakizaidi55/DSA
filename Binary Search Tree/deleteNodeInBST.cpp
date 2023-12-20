0f1c1f720bb54144ecb35fa042deddd74ff86277
zakizaidi55 <zakizaidi55@gmail.com>
2023-12-20 14:59:50 +0530

    second method of delete node in BST

--- Binary Search Tree/deleteNodeInBST.cpp
+++ Binary Search Tree/deleteNodeInBST.cpp
@@ -66,3 +66,76 @@ public:
         return root;
     }
 };
+
+
+// ------------------------------------------------------Method 2---------------------------------
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* maxValue(TreeNode* root) {
+        if(root == NULL) {
+            return NULL;
+        }
+
+        TreeNode* temp = root;
+
+        while(temp->right != NULL) {
+            temp = temp->right;
+        }
+
+        return temp;
+    }
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root == NULL) {
+            return NULL;
+        }
+
+        if (root->val == key) {
+            if(root->left == NULL && root->right == NULL) {
+                delete root;
+                return NULL;
+
+            }
+            // root->left == NULL and root->right != NULL
+            else if(!root->left) {
+                TreeNode* child = root->right;
+                delete root;
+                return child;
+            }
+
+            else if(!root->right) {
+                TreeNode* child = root->left;
+                delete root;
+                return child;
+            }
+
+            else {
+                TreeNode* maxi = maxValue(root->left);
+                root->val = maxi->val;
+                root->left = deleteNode(root->left, maxi->val);
+                return root;
+            }
+        }
+
+        else if(root->val < key) {
+            root->right = deleteNode(root->right, key);
+        } 
+
+        else {
+            root->left = deleteNode(root->left, key);
+        }
+
+        return root;
+    }
+};
