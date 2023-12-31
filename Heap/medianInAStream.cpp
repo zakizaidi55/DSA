0e3b59b4e480181c5fa46772843ace7f2cc47c69
zakizaidi55 <zakizaidi55@gmail.com>
2023-12-31 18:50:51 +0530

    add one more method in find median in a stream

--- Heap/medianInAStream.cpp
+++ Heap/medianInAStream.cpp
@@ -72,3 +72,51 @@ int main() {
 
   return 0;
 }
+
+
+// =======================================Method 2===============================
+class MedianFinder {
+public:
+    priority_queue<int>maxHeap;
+    priority_queue<int, vector<int>, greater<int> >minHeap;
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        if(maxHeap.size() == 0 || num < maxHeap.top()) {
+            maxHeap.push(num);
+        }
+
+        else {
+            minHeap.push(num);
+        }
+
+        // both heap must have same number of element
+        // if not maxHeap must have one more element then min heap
+        if(maxHeap.size() < minHeap.size()) {
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
+
+        else if(maxHeap.size() > minHeap.size() + 1) {
+            minHeap.push(maxHeap.top());
+            maxHeap.pop();
+        }
+    }
+    
+    double findMedian() {
+        if(maxHeap.size() == minHeap.size()) {
+            return (maxHeap.top() + minHeap.top())/2.0;
+        }
+
+        return maxHeap.top();
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
