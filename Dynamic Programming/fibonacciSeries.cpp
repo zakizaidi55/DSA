cd422af460803c3691d4d69fa2990938472ec3aa
zakizaidi55 <zakizaidi55@gmail.com>
2024-01-06 18:43:30 +0530

    add space optimisation code

--- Dynamic Programming/fibonacciSeries.cpp
+++ Dynamic Programming/fibonacciSeries.cpp
@@ -58,6 +58,41 @@ public:
 
     }
 
+    // some spave optimisation
+    int solveUsingTabSpaceOptimise(int n) {
+        // step1: create a dp array
+        // vector<int>dp(n+1, -1);
+
+        // step2: analyse the base case and fil the dp array
+        int prev = 0;
+        if(n == 0) {
+            return n;
+        }
+
+        if(n == 1) {
+            return 1;
+        }
+        int curr = 1;
+
+        // step3: fill the remaining dp array
+        int ans;
+        for(int i=2; i<=n;i++) {
+            // copy paste the recursive relation
+            // replace the recursive calls with dp array
+            // make sure dp arrays is using looping variable "i"
+            // dp[i] = dp[i-1] + dp[i-2];
+            ans = curr + prev;
+
+            // catch->important step bhul jate hai
+            prev = curr;
+            curr = ans;
+        }
+
+        // return ans
+        return ans;
+    }
+
+
     int fib(int n) {
         // int ans = solveUsingRec(n);
         // return ans;
