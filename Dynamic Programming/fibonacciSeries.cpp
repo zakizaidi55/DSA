09b61a99b80747203d2127dc58bec0057cc012d9
zakizaidi55 <zakizaidi55@gmail.com>
2024-01-06 18:25:41 +0530

    Fibonnaci series using Rec and DP

---
+++ Dynamic Programming/fibonacciSeries.cpp
@@ -0,0 +1,74 @@
+class Solution {
+public:
+    int solveUsingRec(int n) {
+        // base case
+        if(n == 0 || n == 1) {
+            return n;
+        }
+
+        int ans = fib(n-1) + fib(n-2);
+
+        return ans;
+    
+    }
+
+    // top down approach
+    // rec + memoisation approach
+
+    int solveUsigMem(int n, vector<int>&dp) {
+        // base case
+        if(n == 0 || n == 1) {
+            return n;
+        }
+
+        // step3: if answer already exist then return 
+        if(dp[n] != -1) {
+            return dp[n];
+        }
+        
+        // step2: store and return dp array
+        dp[n] = solveUsigMem(n-1, dp) + solveUsigMem(n-2, dp);
+
+        return dp[n];
+    }
+
+    // solve using tabulation method
+    int solveUsingTab(int n) {
+        // step1: create a dp array
+        vector<int>dp(n+1, -1);
+
+        // step2: analyse the base case and fil the dp array
+        dp[0] = 0;
+        if(n == 0) {
+            return dp[0];
+        }
+        dp[1] = 1;
+
+        // step3: fill the remaining dp array
+
+        for(int i=2; i<=n;i++) {
+            // copy paste the recursive relation
+            // replace the recursive calls with dp array
+            // make sure dp arrays is using looping variable "i"
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        // return ans
+        return dp[n];
+
+    }
+
+    int fib(int n) {
+        // int ans = solveUsingRec(n);
+        // return ans;
+
+        // step1: create DP array
+        vector<int>dp(n+1, -1);
+        // int ans = solveUsigMem(n, dp);
+        int ans = solveUsingTab(n);
+        return ans;
+    }
+
+
+
+};
