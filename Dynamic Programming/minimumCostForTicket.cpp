81fe2296fbdacc7866e60d0f8cebf4d25310c05e
zakizaidi55 <zakizaidi55@gmail.com>
2024-01-23 18:13:15 +0530

    min cost for ticket using recursion and memoisation

---
+++ Dynamic Programming/minimumCostForTicket.cpp
@@ -0,0 +1,75 @@
+class Solution {
+public:
+    int minCostTicketsHelper(vector<int>& days, vector<int>& costs, int i) {
+        // base case
+        if(i >= days.size())
+            return 0;
+
+        // sol for one case
+        // one day pass
+        int cost1 = costs[0] + minCostTicketsHelper(days, costs, i+1);
+
+        // 7 day pass
+        int passEndDay = days[i] + 7 - 1;
+        int j = i;
+        while(j < days.size() && days[j] <= passEndDay) {
+            j++;
+        }
+        int cost7 = costs[1] + minCostTicketsHelper(days, costs, j);
+
+
+        // 30 day pass
+        passEndDay = days[i] + 30 - 1;
+        j = i;
+        while(j < days.size() && days[j] <= passEndDay) {
+            j++;
+        }
+        int cost30 = costs[2] + minCostTicketsHelper(days, costs, j);
+
+        
+        return min(cost1, min(cost7,cost30));
+    }
+
+
+    int solveUsingMem(vector<int>& days, vector<int>& costs, int i, vector<int>&dp) {
+        // base case
+        if(i >= days.size())
+            return 0;
+
+        if(dp[i] != -1)
+            return dp[i];
+        // sol for one case
+        // one day pass
+        int cost1 = costs[0] + solveUsingMem(days, costs, i+1, dp);
+
+        // 7 day pass
+        int passEndDay = days[i] + 7 - 1;
+        int j = i;
+        while(j < days.size() && days[j] <= passEndDay) {
+            j++;
+        }
+        int cost7 = costs[1] + solveUsingMem(days, costs, j, dp);
+
+
+        // 30 day pass
+        passEndDay = days[i] + 30 - 1;
+        j = i;
+        while(j < days.size() && days[j] <= passEndDay) {
+            j++;
+        }
+        int cost30 = costs[2] + solveUsingMem(days, costs, j, dp);
+
+        
+        dp[i] = min(cost1, min(cost7,cost30));
+
+        return dp[i];
+    }
+    
+
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        int n = days.size();
+        vector<int>dp(n+1, -1);
+        // return minCostTicketsHelper(days, costs, 0);
+        return solveUsingMem(days, costs, 0, dp);
+    }
+};
