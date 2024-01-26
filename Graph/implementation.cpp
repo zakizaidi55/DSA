8e54079b7a045f20bd0645df1505469350bf2315
zakizaidi55 <zakizaidi55@gmail.com>
2024-01-26 21:08:34 +0530

    BFS and DFS traversal

--- Graph/implementation.cpp
+++ Graph/implementation.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include<list>
+#include<queue>
 #include<unordered_map>
 using namespace std;
 
@@ -38,11 +39,13 @@ using namespace std;
 //     }
 // };
 
+template <typename T>
+
 class Graph {
   public:
-  unordered_map<int, list<pair<int, int> > >adjList;
+  unordered_map<T, list<pair<T, int> > >adjList;
    
-    void addEdge(int u, int v, int wt, bool direction) {
+    void addEdge(T u, T v, int wt, bool direction) {
         // direction = 0 ->undirected
         // direction = 1-> directed
         if(direction == 1) {
@@ -59,33 +62,92 @@ class Graph {
 
         }
 
-        cout << endl << " printing adjlist " << endl;
-        printAdjList();
+        // cout << endl << " printing adjlist " << endl;
+        // printAdjList();
     }
 
     void printAdjList() {
         for(auto i:adjList) {
             cout << i.first << " : { " ;
 
-            for(pair<int, int>p: i.second) {
+            for(pair<T, int>p: i.second) {
                 cout << " { " << p.first << " , " <<p.second << " } " ;
             }
 
             cout << " } " << endl;
         }
     }
+
+
+    void bfsTraversal(T src, unordered_map<T, bool>&visited) {
+        // adjlist already bni hui h
+        // visited
+        
+        // queue
+        queue<T>q;
+
+        // initial node
+        q.push(src);
+        visited[src] = true;
+
+        while(!q.empty()) {
+            T frontNode = q.front();
+            cout << frontNode << " ";
+            q.pop();
+
+            // go to neighbour
+            for(auto nbr : adjList[frontNode]) {
+                T nbrData = nbr.first;
+
+                if(!visited[nbrData]) {
+                    q.push(nbrData);
+                    visited[nbrData] = true;
+                }
+            }
+        }
+    }
+
+
+    void dfsTraversal(T src, unordered_map<T, bool>&vis) {
+        vis[src] = true;
+        cout << src << " ";
+
+        for(auto nbr:adjList[src] ) {
+            T nbrData = nbr.first;
+            if(!vis[nbrData]) {
+                dfsTraversal(nbrData, vis);
+            }
+        } 
+    }
 };
 
 
 int main() {
-  Graph g;
+  Graph<char> g;
   // g.addEdge(0,1,1);
   // g.addEdge(1,2,1);
   // g.addEdge(1,3,1);
   // g.addEdge(2,3,1);
-  g.addEdge(0,1,5,0);
-  g.addEdge(1,2,10,0);
-  g.addEdge(1,3,20,0);
-  g.addEdge(2,3,50,0);
+  g.addEdge('a','b',5,0);
+  g.addEdge('a','c',10,0);
+
+  g.addEdge('c','d',20,0);
+
+  g.addEdge('c','e',50,0);
+  g.addEdge('d','e',50,0);
+  g.addEdge('e','f',50,0);
+
+  unordered_map<char, bool>visited;
+//   for(char node='a'; node<='f'; node++) {
+//     if(!visited[node]) {
+//         g.bfsTraversal(node, visited);
+//     }
+//   }
+ for(char node='a'; node<='f'; node++) {
+    if(!visited[node]) {
+        g.dfsTraversal('a', visited);
+    }
+ }
+  
   return 0;
 }
