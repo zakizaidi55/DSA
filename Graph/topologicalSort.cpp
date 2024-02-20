49af6f9d945af7eb7fe507f9218858281ce47aa9
zakizaidi55 <zakizaidi55@gmail.com>
2024-02-20 14:53:43 +0530

    topological sort using BFS

--- Graph/topologicalSort.cpp
+++ Graph/topologicalSort.cpp
@@ -3,6 +3,7 @@
 #include<map>
 #include<list>
 #include<stack>
+#include<queue>
 using namespace std;
 
 class Graph{
@@ -43,6 +44,38 @@ class Graph{
 
             st.push(src);
         }
+
+        void topoSortBfs(int n) {
+            queue<int>q;
+            map<int, int>indegree;
+            for(auto i: adjList) {
+                for(auto nbr: i.second) {
+                    indegree[nbr]++;
+                }
+            }
+
+            // put all the indegree wali node into queue
+            for(int node=0; node<n; node++) {
+                if(indegree[node] == 0) {
+                    q.push(node);
+                }
+            }
+
+            while(!q.empty()) {
+                int frontNode = q.front();
+                q.pop();
+                cout << frontNode << " , " ;
+                for(auto nbr:adjList[frontNode]) {
+                    indegree[nbr]--;
+
+                    // check for zero
+                    if(indegree[nbr] == 0) {
+                        q.push(nbr);
+                    }
+                }
+            }
+
+        }
 };
 
 int main() {
@@ -50,28 +83,30 @@ int main() {
     g.addEdge(0,1,1);
     g.addEdge(1,2,1);
     g.addEdge(2,3,1);
-    g.addEdge(3,4,1);
+    g.addEdge(2,4,1);
     g.addEdge(3,5,1);
-    g.addEdge(4,6,1);
+    g.addEdge(4,5,1);
     g.addEdge(5,6,1);
-    g.addEdge(6,7,1);
+    g.addEdge(5,7,1);
     g.printAdjList();
     int n = 8; 
     map<int, bool> visited;
 
-    stack<int>st;
+    // stack<int>st;
 
-    for(int node=0; node<n; node++) {
-        if(!visited[node]) {
-            g.topoSortDfs(node, visited, st);
-        }
-    }
+    // for(int node=0; node<n; node++) {
+    //     if(!visited[node]) {
+    //         g.topoSortDfs(node, visited, st);
+    //     }
+    // }
 
-    cout << "printing the topo order " << endl;
-    while(!st.empty()) {
-        cout << st.top() << endl;
-        st.pop();
-    }
+    // cout << "printing the topo order " << endl;
+    // while(!st.empty()) {
+    //     cout << st.top() << endl;
+    //     st.pop();
+    // }
+
+    g.topoSortBfs(n);
     return 0;
 
 }
