1dd45b286005b9498b8dae1311bc1c822b764c00
zakizaidi55 <zakizaidi55@gmail.com>
2024-03-14 16:42:35 +0530

    Second approach using map with sliding window

--- binarySubArrWithSum.cpp
+++ binarySubArrWithSum.cpp
@@ -20,3 +20,33 @@ public:
         return ans;
     }
 };
+
+// =====================================================Using Map==============================================
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        unordered_map<int, int>mp;
+        int ans = 0;
+        int prefixSum[nums.size()];
+
+        prefixSum[0] = nums[0];
+
+        for(int i=1; i<nums.size(); i++) {
+            prefixSum[i] = nums[i] + prefixSum[i-1];
+        }
+
+        for(int i=0; i<nums.size(); i++) {
+            if(prefixSum[i] == goal) {
+                ans++;
+            }
+
+            if(mp.find(prefixSum[i] - goal) != mp.end()) {
+                ans += mp[prefixSum[i]-goal];
+            }
+
+            mp[prefixSum[i]]++;
+        }
+
+        return ans;
+    }
+};
