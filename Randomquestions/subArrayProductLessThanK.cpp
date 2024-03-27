a10fcce61d309e90ff6ad6a09b03ff7d1e00f82b
zakizaidi55 <zakizaidi55@gmail.com>
2024-03-27 15:15:13 +0530

    added method 2 for the sub array product less than k

--- Randomquestions/subArrayProductLessThanK.cpp
+++ Randomquestions/subArrayProductLessThanK.cpp
@@ -24,3 +24,32 @@ public:
         return count;
     }
 };
+
+
+// --------------------------Method 2-----------------------------------
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        int left = 0;
+        int right = 0;
+        int product = 1;
+        int count = 0;
+
+        int n = nums.size();
+        if(k <= 1) {
+            return 0;
+        }
+
+        while(right < n){
+            product *= nums[right];
+            while(product >= k) {
+                product = product/nums[left];
+                left++;
+            }
+            count += 1 + (right-left);
+            right++;
+        }
+
+        return count;
+    }
+};
