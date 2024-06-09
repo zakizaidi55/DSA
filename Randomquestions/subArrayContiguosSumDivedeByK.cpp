c4fcf0e02fe2b27353bf7bb53278bdb66d970f94
zakizaidi55 <zakizaidi55@gmail.com>
2024-06-09 23:41:55 +0530

    Subarray Sums Divisible by K

---
+++ Randomquestions/subArrayContiguosSumDivedeByK.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        unordered_map<int,int>mp;
+        mp[0] = 1; //initially if we get % 0 it means
+
+        int ans = 0;
+        int sum = 0;
+        for(int i=0; i<nums.size(); i++) {
+            sum += nums[i];
+            int rem = sum%k;
+            // catch 
+            if(rem < 0) {
+                rem += k;
+            }
+            if(mp.find(rem) != mp.end()) {
+                ans += mp[rem];
+            }         
+
+            mp[rem]++;   
+        }
+
+        return ans;
+    }
+};
