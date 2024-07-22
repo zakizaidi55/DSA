990030df518eb05813099ffd809a5e649283b8a4
zakizaidi55 <zakizaidi55@gmail.com>
2024-07-22 17:40:36 +0530

    Sort the people

---
+++ Randomquestions/sortThePeople.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        vector<pair<int, string> >v;
+        for(int i=0; i<names.size(); i++) {
+            v.push_back({heights[i], names[i]});
+        }
+
+        sort(v.rbegin(), v.rend());
+
+        vector<string>ans;
+        for(auto it: v) {
+            ans.push_back(it.second);
+        }
+
+        return ans;
+    }
+};
