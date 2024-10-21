42fc903409804ae2d6b8c3b7275fa00d5796c750
zakizaidi55 <zakizaidi55@gmail.com>
2024-10-21 17:20:18 +0530

    Split a String Into the Max Number of Unique Substrings

---
+++ Recursion/splitAStringIntoMaxNumber.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+     void solve(string& s, int idx, unordered_set<string>& st, int currCount, int& maxCount) {
+        if(idx == s.length()) {
+            maxCount = max(maxCount, currCount);
+        }
+
+        for(int j = idx; j < s.length(); j++) {
+            string sub = s.substr(idx, j-idx+1);
+            if(st.find(sub) == st.end()) {
+                //not found
+                st.insert(sub);
+                solve(s, j+1, st, currCount+1, maxCount);
+
+                // catch-> backtrack yaha galti kari thi
+                st.erase(sub);
+            }
+        }
+    }
+
+    int maxUniqueSplit(string s) {
+        unordered_set<string> st;
+        int maxCount  = 0;
+        int currCount = 0;
+        solve(s, 0, st, currCount, maxCount);
+
+        return maxCount;
+    }
+};
