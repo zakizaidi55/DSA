d9c5d4a1fa4118dad2a827ab09215131c3a792c1
zakizaidi55 <zakizaidi55@gmail.com>
2024-11-06 23:23:11 +0530

    Find If Array can be sorted

---
+++ Randomquestions/findArrayCanBeSorted.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool canSortArray(vector<int>& nums) {
+        int n = nums.size();
+        bool isSwapped = true;
+        for(int i=0; i<n; i++) {
+            isSwapped = false;
+            for(int j=0; j<n-i-1; j++ ) {
+                if(nums[j] > nums[j+1]) {   
+                    if(__builtin_popcount(nums[j]) == __builtin_popcount(nums[j+1])) {
+                        swap(nums[j], nums[j+1]);
+                        isSwapped = true;
+                    }
+
+                    else {
+                        return false;
+                    }
+                }
+            }
+
+            if(isSwapped == false)
+                break;
+        }
+
+        return true;
+    }
+};
