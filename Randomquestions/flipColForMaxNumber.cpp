d9b42a94636b4d9c50182385ea33ab88127671ba
zakizaidi55 <zakizaidi55@gmail.com>
2024-11-22 15:17:14 +0530

    Flip Columns For Maximum Number of Equal Rows

---
+++ Randomquestions/flipColForMaxNumber.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int maxRows = 0;
+
+        for(auto&currRow: matrix) {
+            vector<int>inverted(n);
+
+            for(int col=0; col<n; col++) {
+                inverted[col] = currRow[col] == 0 ? 1 : 0;
+            }
+
+            int count = 0;
+            
+            // travel all the rows again and find the inverted or same
+
+            for(auto&row: matrix) {
+                if(row == currRow || row == inverted) {
+                    count++;
+                }
+            }
+
+            maxRows = max(maxRows, count);
+        }
+
+        return maxRows;
+    }
+};
