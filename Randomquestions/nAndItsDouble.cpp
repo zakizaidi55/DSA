36281c255e3bb3f5f0af4173fcbdffc4d4bb50fe
Zaki Zaidi <zakizaidi@Zakis-MacBook-Pro.local>
2024-12-01 14:50:47 +0530

    Check If N and Its Double Exist

---
+++ Randomquestions/nAndItsDouble.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        unordered_map<int, int>mp;
+
+        for(int i=0; i<arr.size(); i++) {
+            mp[arr[i]] = i;
+        }
+
+        for(int i=0; i<arr.size(); i++) {
+            if(mp.find(2*arr[i]) != mp.end() && mp[2*arr[i]] != i) 
+                return true;
+        }
+
+        return false;
+    }
+};
