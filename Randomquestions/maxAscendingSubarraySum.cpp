956adb62f6d4ee1af49ef321cdc9ae7ce525e032
zakizaidi55 <zakizaidi55@gmail.com>
2025-02-04 10:27:29 +0530

    Maximum Ascending Subarray Sum

---
+++ Randomquestions/maxAscendingSubarraySum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+        int ans = nums[0];
+        int sum = nums[0];
+        for(int i=1; i<nums.size(); i++) {
+            if(nums[i] > nums[i-1]) {
+                sum += nums[i];
+            }
+
+            else {
+                sum = nums[i];
+            }
+
+            ans = max(ans, sum);
+        }
+
+        return ans;
+    }
+};
