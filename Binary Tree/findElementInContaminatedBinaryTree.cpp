341f4f6bd09844ff4d31fec098860e220de8a041
zakizaidi55 <zakizaidi55@gmail.com>
2025-02-21 10:50:30 +0530

    Find Elements in a Contaminated Binary Tree using DFS

--- Binary Tree/findElementInContaminatedBinaryTree.cpp
+++ Binary Tree/findElementInContaminatedBinaryTree.cpp
@@ -53,3 +53,47 @@ public:
  * FindElements* obj = new FindElements(root);
  * bool param_1 = obj->find(target);
  */
+
+
+ /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class FindElements {
+    public:
+        unordered_set<int>st;
+    
+        void dfs(TreeNode* root, int val) {
+    
+            if(!root) 
+                return;
+            root->val = val;
+            st.insert(val);
+    
+            dfs(root->left, 2*val+1);
+            dfs(root->right, 2*val+2);
+        }
+    
+    
+        FindElements(TreeNode* root) {
+            st.clear();
+            dfs(root, 0);
+        }   
+        
+        bool find(int target) {
+            return st.count(target);
+        }
+    };
+    
+    /**
+     * Your FindElements object will be instantiated and called as such:
+     * FindElements* obj = new FindElements(root);
+     * bool param_1 = obj->find(target);
+     */
