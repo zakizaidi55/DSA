e192a196073deb8d32e2a8bcf0a36f93cff2158a
zakizaidi55 <zakizaidi55@gmail.com>
2025-04-20 17:09:01 +0530

    Rabbits in forest

---
+++ Randomquestions/rabbitsInForest.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numRabbits(vector<int>& answers) {
+        unordered_map<int, int>mp;
+
+        for(int a: answers) {
+            mp[a]++;
+        }
+
+        int total = 0;
+        
+        for (auto& it : mp) {
+            int x  = it.first;  
+            int count = it.second; 
+
+            int groupSize = x + 1;
+            int groups = ceil((double)count / groupSize);
+
+            total += groups * groupSize;
+        }
+
+        return total;
+    }
+};
+
+
