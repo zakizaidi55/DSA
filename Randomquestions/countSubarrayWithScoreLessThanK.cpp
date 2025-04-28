d7dadd65b0423c5f7ec42094e441a0590de82908
zakizaidi55 <zakizaidi55@gmail.com>
2025-04-28 11:47:29 +0530

    Count Subarrays With Score Less Than K

---
+++ Randomquestions/countSubarrayWithScoreLessThanK.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, long long k) {
+        long long result = 0;
+        long long sum = 0;
+        int i = 0;
+        int j = 0;
+
+        while(j < nums.size()) {
+            sum += nums[j];
+
+            while(i <= j && sum * (j-i+1) >= k) {
+                sum -= nums[i];
+                i++;
+            }
+
+            result += (j-i+1);
+            j++;
+        }
+
+        return result;
+    }
+};
+
+
+
+
+
+
