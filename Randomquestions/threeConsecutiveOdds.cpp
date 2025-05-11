9c1eb3fd028390cb124d4ee0f486378746565197
zakizaidi55 <zakizaidi55@gmail.com>
2025-05-11 12:51:10 +0530

    Three consecutive odds approach2

--- Randomquestions/threeConsecutiveOdds.cpp
+++ Randomquestions/threeConsecutiveOdds.cpp
@@ -43,3 +43,29 @@ public:
         return false;
     }
 };
+
+
+----------------------------------------------------------
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+
+        if(arr.size() < 3)
+            return false;
+        int oddCount = 0;
+        for(int i=0; i<arr.size(); i++) {
+            if(arr[i]&1)
+                oddCount++;
+            
+            else
+                oddCount = 0;
+            
+            if(oddCount == 3)
+                return true;
+        }
+
+        return false;
+    }
+};
+
+
