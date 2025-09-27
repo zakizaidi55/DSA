e55f08ea556396490e3819fb7c31e6cdfc5ce6fe
zakizaidi55 <zakizaidi55@gmail.com>
2025-09-27 17:23:04 +0530

    Largest Triangle Area

---
+++ Randomquestions/largestTriangleArea.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    double largestTriangleArea(vector<vector<int>>& points) {
+        int n = points.size();
+        double maxArea = 0.0;
+
+        for(int i=0; i<n; i++) {
+            for(int j=i+1; j<n; j++) {
+                for(int k=j+1; k<n; k++) {
+                    double x1 = points[i][0];
+                    double y1 = points[i][1];
+
+                    double x2 = points[j][0];
+                    double y2 = points[j][1];
+
+                    double x3 = points[k][0];
+                    double y3 = points[k][1];
+
+
+                    double a = hypot(x2-x1, y2-y1);
+                    double b = hypot(x3-x2, y3-y2);
+                    double c = hypot(x3-x1, y3-y1);
+
+                    double s = (a+b+c) * 0.5;
+
+                    double sheronFormula = sqrt(s* (s-a) * (s-b) * (s-c));
+
+                    maxArea = max(maxArea, sheronFormula);
+                }
+            }
+        }
+
+        return maxArea;
+    }
+};
+
+
