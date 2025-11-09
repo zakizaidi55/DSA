71de1128528c93ce00e00db98e7e9e642f3911db
zakizaidi55 <zakizaidi55@gmail.com>
2025-11-09 23:42:58 +0530

    Count Operations to Obtain Zero

---
+++ Randomquestions/countOperationsToObtainZero.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        if(num2 > num1)
+            swap(num1, num2);
+        
+        int count = 0;
+        while(num1 > 0 && num2 > 0) {
+            count += num1/num2;
+            num1 %= num2;
+            swap(num1, num2);
+        }
+
+        return count;
+    }
+};
+
+
