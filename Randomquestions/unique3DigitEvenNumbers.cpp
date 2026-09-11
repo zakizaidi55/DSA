7810070862fb014ff556614ce13fdf02df99d419
zakizaidi55 <zakizaidi55@gmail.com>
2026-09-11 17:05:25 +0530

    Unique 3-Digit Even Numbers

---
+++ Randomquestions/unique3DigitEvenNumbers.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int totalNumbers(vector<int>& digits) {
+        unordered_set<int>st;
+        int n = digits.size();
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                for(int k=0; k<n; k++) {
+                    if(i == j || j == k || i == k) 
+                        continue;
+                    int num = digits[i]*100 + digits[j] * 10 + digits[k];
+                    if(num >= 100 && num%2 == 0) 
+                        st.insert(num);
+                }
+            }
+        }
+
+        return st.size();
+    }
+};
